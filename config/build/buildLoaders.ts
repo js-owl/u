bd26b148423aa17016c6e80c071d403482aa1001
Alex K <aklimovskikh@yandex.ru>
2024-12-06 15:15:23 +0300

    05_main-css

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -1,10 +1,11 @@
+import MiniCssExtractPlugin from "mini-css-extract-plugin";
 import webpack from "webpack";
 
 export function buildLoaders(): webpack.RuleSetRule[] {
   const ts = { test: /\.tsx?$/, use: "ts-loader", exclude: /node_modules/ };
   const css = {
     test: /\.s[ac]ss$/i,
-    use: ["style-loader", "css-loader", "sass-loader"],
+    use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
   };
   return [ts, css];
 }
--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -1,6 +1,7 @@
 import webpack from "webpack";
 import HTMLWebpackPlugin from "html-webpack-plugin";
 import { BuildOptions } from "./types/config";
+import MiniCssExtractPlugin from "mini-css-extract-plugin";
 
 export function buildPlugins({
   paths,
@@ -8,5 +9,9 @@ export function buildPlugins({
   return [
     new webpack.ProgressPlugin(),
     new HTMLWebpackPlugin({ template: paths.html }),
+    new MiniCssExtractPlugin({
+      filename: "css/[name].[contenthash].css",
+      chunkFilename: "css/[name].[contenthash].css",
+    }),
   ];
 }
