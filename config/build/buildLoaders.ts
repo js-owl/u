f93493d11e1416d28bfcb742066c57c69826691b
Alex K <aklimovskikh@yandex.ru>
2024-12-06 13:34:00 +0300

    04_css

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -2,5 +2,9 @@ import webpack from "webpack";
 
 export function buildLoaders(): webpack.RuleSetRule[] {
   const ts = { test: /\.tsx?$/, use: "ts-loader", exclude: /node_modules/ };
-  return [ts];
+  const css = {
+    test: /\.s[ac]ss$/i,
+    use: ["style-loader", "css-loader", "sass-loader"],
+  };
+  return [ts, css];
 }
--- src/c/Counter.tsx
+++ src/c/Counter.tsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import "./Counter.scss";
 
 export const Counter = () => {
   const [count, setCount] = useState(0);
