80d2aa0305e1a0e6b10a091c1ca4de94e8b26018
Alex K <aklimovskikh@yandex.ru>
2024-12-07 18:57:55 +0300

    06_main-about

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
-import { Counter } from "./c/Counter";
 import "./index.scss";
+import { Link, Routes, Route } from "react-router-dom";
+import MainPage from "./p/MainPage/MainPage";
+import AboutPage from "./p/AboutPage/AboutPage";
 
 const App = () => {
   return (
     <div className="app">
-      <Counter />
+      <Link to={"/"}>Main</Link>
+      <Link to={"/about"}>About</Link>
+      <Routes>
+        <Route path={"/"} element={<MainPage />} />
+        <Route path={"/about"} element={<AboutPage />} />
+      </Routes>
     </div>
   );
 };
