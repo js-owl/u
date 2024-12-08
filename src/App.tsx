087bb5baa9c3976b5a2dedbe7c66fff04bcd630f
Alex K <aklimovskikh@yandex.ru>
2024-12-08 14:44:40 +0300

    07_add-toggle-button

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,21 @@ import "./s/index.scss";
 import { Link, Routes, Route } from "react-router-dom";
 import { MainPageAsync } from "./p/MainPage/MainPage.async";
 import { AboutPageAsync } from "./p/AboutPage/AboutPage.async";
-import { Suspense } from "react";
+import { Suspense, useState } from "react";
+
+export enum Theme {
+  LIGHT = "light",
+  DARK = "dark",
+}
 
 const App = () => {
+  const [theme, setTheme] = useState<Theme>(Theme.LIGHT);
+  const toogleTheme = () => {
+    setTheme(theme == Theme.DARK ? Theme.LIGHT : Theme.DARK);
+  };
   return (
-    <div className="app dark">
+    <div className={`app ${theme}`}>
+      <button onClick={toogleTheme}>Toggle</button>
       <Link to={"/"}>Main</Link>
       <Link to={"/about"}>About</Link>
       <Suspense fallback={<div>Loading...</div>}>
