50aaf98a611ee3d4a65fecf93761baf4d5d39574
Alex K <aklimovskikh@yandex.ru>
2024-12-16 22:22:23 +0300

    12_add-navbar

--- src/7_app/App.tsx
+++ src/7_app/App.tsx
@@ -1,6 +1,6 @@
 import "./s/index.scss";
-import { Link } from "react-router-dom";
 import { classNames } from "1_shared/libs/classNames/classNames";
+import { Navbar } from "4_widgets/Navbar";
 import { useTheme } from "7_app/providers/ThemeProvider";
 import { AppRouter } from "./providers/router";
 
@@ -8,10 +8,9 @@ const App = () => {
   const { theme, toggleTheme } = useTheme();
   return (
     <div className={classNames("app", {}, [theme])}>
-      <button onClick={toggleTheme}>Toggle</button>
-      <Link to={"/"}>Main</Link>
-      <Link to={"/about"}>About</Link>
+      <Navbar />
       <AppRouter />
+      <button onClick={toggleTheme}>Toggle</button>
     </div>
   );
 };
