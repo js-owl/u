8f0402d548a1f60d5540ab315f99c26ede619d38
Alex K <aklimovskikh@yandex.ru>
2024-12-22 10:56:32 +0300

    20_test-classNames

--- src/1_shared/libs/classNames/classNames.test.ts
+++ src/1_shared/libs/classNames/classNames.test.ts
@@ -1,5 +1,20 @@
+import { classNames } from "./classNames";
+
 describe("classNames", () => {
-  test("test", () => {
-    expect(true).toBe(true);
+  test("first param", () => {
+    expect(classNames("c1")).toBe("c1");
+  });
+  test("second param", () => {
+    expect(classNames("c1", { c2: true, c3: false })).toBe("c1 c2");
+  });
+  test("second param undef", () => {
+    expect(classNames("c1", { c2: true, c3: undefined }, ["c4", "c5"])).toBe(
+      "c1 c4 c5 c2"
+    );
+  });
+  test("third param", () => {
+    expect(classNames("c1", { c2: true, c3: false }, ["c4", "c5"])).toBe(
+      "c1 c4 c5 c2"
+    );
   });
 });
