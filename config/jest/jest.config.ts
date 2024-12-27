3401c52ffd658accae9e9b4732211d5d29ce9981
Alex K <aklimovskikh@yandex.ru>
2024-12-27 10:37:19 +0300

    24_test-button

--- config/jest/jest.config.ts
+++ config/jest/jest.config.ts
@@ -3,7 +3,7 @@
  * https://jestjs.io/docs/configuration
  */
 
-// import type { Config } from "jest";
+import path from "path";
 
 export default {
   clearMocks: true,
@@ -22,6 +22,12 @@ export default {
   ],
   rootDir: "../../",
   testMatch: ["<rootDir>src/**/*(*.)@(spec|test).[tj]s?(x)"],
+  modulePaths: ["<rootDir>src"],
+  setupFilesAfterEnv: ["<rootDir>config/jest/setupTests.ts"],
+  moduleNameMapper: {
+    "\\.s?css$": "identity-obj-proxy",
+    "\\.svg": path.resolve(__dirname, "jestEmptyComponent.tsx"),
+  },
 
   // All imported modules in your tests should be mocked automatically
   // automock: false,
