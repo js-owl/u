4afaa7ba89e402cc9cfa4dfebf26420d4f02230a
Alex K <aklimovskikh@yandex.ru>
2024-12-29 11:57:57 +0300

    25_storybook-button

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -1,6 +1,6 @@
-import MiniCssExtractPlugin from "mini-css-extract-plugin";
 import webpack from "webpack";
 import { BuildOptions } from "./types/config";
+import { buildCssLoader } from "./loaders/buildCssLoader";
 
 export function buildLoaders({ isDev }: BuildOptions): webpack.RuleSetRule[] {
   const svg = { test: /\.svg$/, use: ["@svgr/webpack"] };
@@ -9,23 +9,6 @@ export function buildLoaders({ isDev }: BuildOptions): webpack.RuleSetRule[] {
     use: [{ loader: "file-loader" }],
   };
   const ts = { test: /\.tsx?$/, use: "ts-loader", exclude: /node_modules/ };
-  const css = {
-    test: /\.s[ac]ss$/i,
-    use: [
-      isDev ? "style-loader" : MiniCssExtractPlugin.loader,
-      {
-        loader: "css-loader",
-        options: {
-          modules: {
-            auto: (resPath: string) => Boolean(resPath.includes(".module.")),
-            localIdentName: isDev
-              ? "[path][name]__[local]--[hash:base64:5]"
-              : "[hash:base64:8]",
-          },
-        },
-      },
-      "sass-loader",
-    ],
-  };
+  const css = buildCssLoader(isDev);
   return [svg, file, ts, css];
 }
