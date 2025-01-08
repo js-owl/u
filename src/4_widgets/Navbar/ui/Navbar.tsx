6f1e7d11cecdc32c87f677107263466abb8fc5ad
Alex K <aklimovskikh@yandex.ru>
2025-01-08 12:03:25 +0300

    28_links-to-sidebar

--- src/4_widgets/Navbar/ui/Navbar.tsx
+++ src/4_widgets/Navbar/ui/Navbar.tsx
@@ -1,5 +1,4 @@
 import { classNames } from "1_shared/libs/classNames/classNames";
-import { AppLink, AppLinkTheme } from "1_shared/ui/AppLink/AppLink";
 import cls from "./Navbar.module.scss";
 
 interface NavbarProps {
@@ -7,21 +6,6 @@ interface NavbarProps {
 }
 
 export const Navbar = ({ className }: NavbarProps) => {
-  return (
-    <div className={classNames(cls.Navbar, {}, [className])}>
-      <div className={cls.links}>
-        <AppLink
-          theme={AppLinkTheme.SECONDARY}
-          to={"/"}
-          className={cls.mainLink}
-        >
-          Main
-        </AppLink>
-        <AppLink theme={AppLinkTheme.RED} to={"/about"}>
-          About
-        </AppLink>
-      </div>
-    </div>
-  );
+  return <div className={classNames(cls.Navbar, {}, [className])}></div>;
 };
 export default Navbar;
--- src/4_widgets/Sidebar/ui/Sidebar/Sidebar.tsx
+++ src/4_widgets/Sidebar/ui/Sidebar/Sidebar.tsx
@@ -4,11 +4,15 @@ import { useState } from "react";
 import { ThemeSwitcher } from "4_widgets/ThemeSwitcher";
 import { LangSwithcher } from "4_widgets/LangSwithcher";
 import { Button, ButtonTheme, ButtonSize } from "1_shared/ui/Button/Button";
+import { AppLink, AppLinkTheme } from "1_shared/ui/AppLink/AppLink";
+import { RoutePath } from "1_shared/config/routeConfig/routeConfig";
+import { useTranslation } from "react-i18next";
 interface SidebarProps {
   className?: string;
 }
 export const Sidebar = ({ className }: SidebarProps) => {
   const [collapsed, setCollapsed] = useState(false);
+  const { t } = useTranslation();
   const onToggle = () => {
     setCollapsed((prev) => !prev);
   };
@@ -29,6 +33,22 @@ export const Sidebar = ({ className }: SidebarProps) => {
       >
         {collapsed ? ">" : "<"}
       </Button>
+      <div className={cls.items}>
+        <AppLink
+          theme={AppLinkTheme.SECONDARY}
+          to={RoutePath.main}
+          className={cls.item}
+        >
+          {t("main")}
+        </AppLink>
+        <AppLink
+          theme={AppLinkTheme.RED}
+          to={RoutePath.about}
+          className={cls.item}
+        >
+          {t("about")}
+        </AppLink>
+      </div>
       <div className={cls.switchers}>
         <ThemeSwitcher />
         <LangSwithcher short={collapsed} className={cls.lang} />
