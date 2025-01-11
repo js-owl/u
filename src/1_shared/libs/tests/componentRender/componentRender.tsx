b14d53c37040573ac96def186fda00f966dd8ab9
Alex K <aklimovskikh@yandex.ru>
2025-01-11 14:57:28 +0300

    30_redux-test

--- src/1_shared/libs/tests/componentRender/componentRender.tsx
+++ src/1_shared/libs/tests/componentRender/componentRender.tsx
@@ -2,21 +2,26 @@ import { ReactNode } from "react";
 import { render } from "@testing-library/react";
 import { I18nextProvider } from "react-i18next";
 import { MemoryRouter } from "react-router-dom";
+import { DeepPartial } from "@reduxjs/toolkit";
 import i18nForTests from "1_shared/config/i18n/i18nForTests";
+import { StateSchema, StoreProvider } from "7_app/providers/StoreProvider";
 
 export interface componentRenderOptions {
   route?: string;
+  initialState?: DeepPartial<StateSchema>;
 }
 
 export function componentRender(
   component: ReactNode,
   options: componentRenderOptions = {}
 ) {
-  const { route = "/" } = options;
+  const { route = "/", initialState } = options;
 
   return render(
-    <MemoryRouter initialEntries={[route]}>
-      <I18nextProvider i18n={i18nForTests}>{component}</I18nextProvider>
-    </MemoryRouter>
+    <StoreProvider initialState={initialState}>
+      <MemoryRouter initialEntries={[route]}>
+        <I18nextProvider i18n={i18nForTests}>{component}</I18nextProvider>
+      </MemoryRouter>
+    </StoreProvider>
   );
 }
