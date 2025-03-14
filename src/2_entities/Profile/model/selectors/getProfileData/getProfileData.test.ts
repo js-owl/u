657eddb0deda72e94ba9415440fcc0bb260c7b4b
Alex K <aklimovskikh@yandex.ru>
2025-03-14 09:26:00 +0300

    43 Test Profile selectors and services

---
+++ src/2_entities/Profile/model/selectors/getProfileData/getProfileData.test.ts
@@ -0,0 +1,28 @@
+import { Country } from "2_entities/Country";
+import { Currency } from "2_entities/Currency";
+import { StateSchema } from "7_app/providers/StoreProvider";
+import { getProfileData } from "./getProfileData";
+
+describe("getProfileData.test", () => {
+  test("should return error", () => {
+    const data = {
+      username: "admin",
+      age: 22,
+      country: Country.Belarus,
+      lastname: "ulbi tv",
+      first: "asd",
+      city: "asf",
+      currency: Currency.USD,
+    };
+    const state: DeepPartial<StateSchema> = {
+      profile: {
+        data,
+      },
+    };
+    expect(getProfileData(state as StateSchema)).toEqual(data);
+  });
+  test("should work with empty state", () => {
+    const state: DeepPartial<StateSchema> = {};
+    expect(getProfileData(state as StateSchema)).toEqual(undefined);
+  });
+});
---
+++ src/2_entities/Profile/model/services/updateProfileData/updateProfileData.test.ts
@@ -0,0 +1,60 @@
+import { TestAsyncThunk } from "1_shared/libs/tests/TestAsyncThunk/TestAsyncThunk";
+import { userActions } from "2_entities/User";
+import { Country } from "2_entities/Country";
+import { Currency } from "2_entities/Currency";
+import { ValidateProfileError } from "2_entities/Profile";
+import { updateProfileData } from "./updateProfileData";
+
+const data = {
+  username: "admin",
+  age: 22,
+  country: Country.Belarus,
+  lastname: "ulbi tv",
+  first: "asd",
+  city: "asf",
+  currency: Currency.USD,
+};
+
+describe("updateProfileData.test", () => {
+  test("success", async () => {
+    const thunk = new TestAsyncThunk(updateProfileData, {
+      profile: {
+        form: data,
+      },
+    });
+
+    thunk.api.put.mockReturnValue(Promise.resolve({ data }));
+
+    const result = await thunk.callThunk();
+
+    expect(thunk.api.put).toHaveBeenCalled();
+    expect(result.meta.requestStatus).toBe("fulfilled");
+    expect(result.payload).toEqual(data);
+  });
+
+  test("error", async () => {
+    const thunk = new TestAsyncThunk(updateProfileData, {
+      profile: {
+        form: data,
+      },
+    });
+    thunk.api.put.mockReturnValue(Promise.resolve({ status: 403 }));
+
+    const result = await thunk.callThunk();
+
+    expect(result.meta.requestStatus).toBe("rejected");
+    expect(result.payload).toEqual([ValidateProfileError.SERVER_ERROR]);
+  });
+
+  test("validate error", async () => {
+    const thunk = new TestAsyncThunk(updateProfileData, {
+      profile: {
+        form: { ...data, lastname: "" },
+      },
+    });
+    const result = await thunk.callThunk();
+
+    expect(result.meta.requestStatus).toBe("rejected");
+    expect(result.payload).toEqual([ValidateProfileError.INCORRECT_USER_DATA]);
+  });
+});
