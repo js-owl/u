7e194604f4fb6bf6e82146e677ac7d74c92caaa5
Alex K <aklimovskikh@yandex.ru>
2025-03-14 09:54:59 +0300

    43 Test Profile slice

---
+++ src/2_entities/Profile/model/slice/profileSlice.test.ts
@@ -0,0 +1,90 @@
+import {
+  profileActions,
+  profileReducer,
+  ProfileShema,
+  updateProfileData,
+  ValidateProfileError,
+} from "2_entities/Profile";
+import { Country } from "2_entities/Country";
+import { Currency } from "2_entities/Currency";
+
+const data = {
+  username: "admin",
+  age: 22,
+  country: Country.Belarus,
+  lastname: "ulbi tv",
+  first: "asd",
+  city: "asf",
+  currency: Currency.USD,
+};
+
+describe("profileSlice.test", () => {
+  test("test set readonly", () => {
+    const state: DeepPartial<ProfileShema> = { readonly: false };
+    expect(
+      profileReducer(state as ProfileShema, profileActions.setReadonly(true))
+    ).toEqual({ readonly: true });
+  });
+
+  test("test cancel edit", () => {
+    const state: DeepPartial<ProfileShema> = { data, form: { username: "" } };
+
+    expect(
+      profileReducer(state as ProfileShema, profileActions.cancelEdit())
+    ).toEqual({
+      readonly: true,
+      validateErrors: undefined,
+      data,
+      form: data,
+    });
+  });
+
+  test("test update profile", () => {
+    const state: DeepPartial<ProfileShema> = { form: { username: "123" } };
+
+    expect(
+      profileReducer(
+        state as ProfileShema,
+        profileActions.updateProfileData({
+          username: "123456",
+        })
+      )
+    ).toEqual({
+      form: { username: "123456" },
+    });
+  });
+
+  test("test update profile service pending", () => {
+    const state: DeepPartial<ProfileShema> = {
+      isLoading: false,
+      validateError: [ValidateProfileError.SERVER_ERROR],
+    };
+
+    expect(
+      profileReducer(state as ProfileShema, updateProfileData.pending)
+    ).toEqual({
+      isLoading: true,
+      validateErrors: undefined,
+    });
+  });
+
+  test("test update profile service fullfiled", () => {
+    const state: DeepPartial<ProfileShema> = {
+      isLoading: true,
+    };
+
+    expect(
+      profileReducer(
+        state as ProfileShema,
+        updateProfileData.fulfilled(data, "")
+      )
+    ).toEqual({
+      isLoading: false,
+      validateErrors: undefined,
+      readonly: true,
+      validateError: undefined,
+      form: data,
+      data,
+    });
+  });
+});
