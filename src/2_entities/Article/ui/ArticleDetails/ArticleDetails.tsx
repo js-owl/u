0f4d3fbfd6e9be3d4736035c7593aab11886b391
Alex K <aklimovskikh@yandex.ru>
2025-03-16 06:06:13 +0300

    49 Add selector: articleDetails

--- src/2_entities/Article/ui/ArticleDetails/ArticleDetails.tsx
+++ src/2_entities/Article/ui/ArticleDetails/ArticleDetails.tsx
@@ -1,4 +1,5 @@
 import { memo, useEffect } from "react";
+import { useSelector } from "react-redux";
 import { classNames } from "1_shared/libs/classNames/classNames";
 import {
   DynamicModuleLoader,
@@ -8,6 +9,12 @@ import { useAppDispatch } from "1_shared/libs/hooks/useAppDispatch/useAppDispatc
 import { fetchArticleById } from "2_entities/Article/model/services/fetchArticleById/fetchArticleById";
 import cls from "./ArticleDetails.module.scss";
 import { articleDetailsReducer } from "../../model/slice/articleDetailsSlice";
+import {
+  getArticleDetailsData,
+  getArticleDetailsError,
+  getArticleDetailsIsLoading,
+} from "../../model/selectors/articleDetails";
+import { Text, TextAlign } from "1_shared/ui/Text/Text";
 
 interface ArticleDetailsProps {
   className?: string;
@@ -20,14 +27,28 @@ const reducers: ReducersList = {
 
 export const ArticleDetails = memo(({ className, id }: ArticleDetailsProps) => {
   const dispatch = useAppDispatch();
+  const article = useSelector(getArticleDetailsData);
+  const isLoading = useSelector(getArticleDetailsIsLoading);
+  const error = useSelector(getArticleDetailsError);
+
   useEffect(() => {
     dispatch(fetchArticleById(id));
   }, [dispatch, id]);
 
+  let content;
+
+  if (isLoading) {
+    content = <div>Loading...</div>;
+  } else if (error) {
+    content = <Text title="error" align={TextAlign.CENTER} />;
+  } else {
+    content = <div>ArticleDetails</div>;
+  }
+
   return (
     <DynamicModuleLoader reducers={reducers} removeAfterUnmount={true}>
       <div className={classNames(cls.ArticleDetails, {}, [className])}>
-        ArticleDetails
+        {content}
       </div>
     </DynamicModuleLoader>
   );
