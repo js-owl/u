b8a1bee83e9629b2e0df8f55bf105c436208f12c
Alex K <aklimovskikh@yandex.ru>
2025-03-22 23:26:02 +0300

    54 ArticleListItemSkeleton

--- src/2_entities/Article/ui/ArticleList/ArticleList.tsx
+++ src/2_entities/Article/ui/ArticleList/ArticleList.tsx
@@ -3,6 +3,7 @@ import { classNames } from "1_shared/libs/classNames/classNames";
 import { Article, ArticleView } from "2_entities/Article/model/types/article";
 import cls from "./ArticleList.module.scss";
 import { ArticleListItem } from "../ArticleListItem/ArticleListItem";
+import { ArticleListItemSkeleton } from "../ArticleListItem/ArticleListItemSkelton";
 
 interface ArticleListProps {
   className?: string;
@@ -11,8 +12,25 @@ interface ArticleListProps {
   view?: ArticleView;
 }
 
+const getSkeletons = (view: ArticleView) => {
+  return new Array(view === ArticleView.SMALL ? 9 : 3)
+    .fill(0)
+    .map((item, index) => (
+      <ArticleListItemSkeleton key={index} className={cls.card} view={view} />
+    ));
+};
+
 export const ArticleList = memo((props: ArticleListProps) => {
   const { className, isLoading, articles, view = ArticleView.SMALL } = props;
+
+  if (isLoading) {
+    return (
+      <div className={classNames(cls.ArticleList, {}, [className, cls[view]])}>
+        {getSkeletons(view)}
+      </div>
+    );
+  }
+
   const renderArticle = (article: Article) => (
     <ArticleListItem
       key={article.id}
