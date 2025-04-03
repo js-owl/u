1935e38b96ba932233f6d6734077b3e83b7646ca
Alex K <aklimovskikh@yandex.ru>
2025-04-03 13:26:06 +0300

    63 Add virtualization in ArticleList.tsx

--- src/2_entities/Article/ui/ArticleList/ArticleList.tsx
+++ src/2_entities/Article/ui/ArticleList/ArticleList.tsx
@@ -58,3 +58,88 @@ export const ArticleList = memo((props: ArticleListProps) => {
     </div>
   );
 });
+
+// Virtualization
+// npm i react-virtualized@9.22.3 --save --force
+// npm i -D @types/react-virtualized@9.21.21
+
+// export const ArticleList = memo((props: ArticleListProps) => {
+//   const {
+//     className,
+//     isLoading,
+//     articles,
+//     view = ArticleView.SMALL,
+//     target,
+//   } = props;
+//   const { t } = useTranslation();
+
+//   const isBig = view === ArticleView.BIG;
+
+//   const itemsPerRow = isBig ? 1 : 3;
+//   const rowCount = isBig
+//     ? articles.length
+//     : Math.ceil(articles.length / itemsPerRow);
+
+//   const rowRenderer = ({ index, isScrolling, key, style }: ListRowProps) => {
+//     const items = [];
+//     const fromIndex = index * itemsPerRow;
+//     const toIndex = Math.min(fromIndex * itemsPerRow, articles.length);
+
+//     for (let i = fromIndex; i < toIndex; i++) {
+//       items.push(
+//         <ArticleListItem
+//           article={articles[i]}
+//           view={view}
+//           className={cls.card}
+//           target={target}
+//           key={`str${i}`}
+//         />
+//       );
+//     }
+
+//     return (
+//       <div key={key} style={style} className={cls.row}>
+//         {items}
+//       </div>
+//     );
+//   };
+
+//   if (!isLoading && !articles.length) {
+//     return (
+//       <div className={classNames(cls.ArticleList, {}, [className, cls[view]])}>
+//         <Text title={t("not found")} size={TextSize.L} />
+//       </div>
+//     );
+//   }
+
+//   return (
+//     <WindowScroller scrollElement={document.getElementById(PAGE_ID) as Element}>
+//       {({
+//         height,
+//         width,
+//         registerChild,
+//         onChildScroll,
+//         isScrolling,
+//         scrollTop,
+//       }) => (
+//         <div
+//           ref={registerChild}
+//           className={classNames(cls.ArticleList, {}, [className, cls[view]])}
+//         >
+//           <List
+//             height={height ?? 700}
+//             rowCount={rowCount}
+//             rowHeight={isBig ? 700 : 330}
+//             rowRenderer={rowRenderer}
+//             width={width ? width - 80 : 700}
+//             autoHeight
+//             onScroll={onChildScroll}
+//             isScrolling={isScrolling}
+//             scrollTop={scrollTop}
+//           />
+//           {isLoading && getSkeletons(view)}
+//         </div>
+//       )}
+//     </WindowScroller>
+//   );
+// });
