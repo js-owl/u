dc292a184293fad204cf64ea3af18345da631804
Alex K <aklimovskikh@yandex.ru>
2025-04-10 08:58:45 +0300

    69 Add createSlice/index.js in scripts

--- config/storybook/preview.js
+++ config/storybook/preview.js
@@ -1,17 +1,17 @@
-import { addDecorator } from "@storybook/react";
-import { StyleDecorator } from "../../src/1_shared/config/storybook/StyleDecorator/StyleDecorator";
-import { ThemeDecorator } from "../../src/1_shared/config/storybook/ThemeDecorator/ThemeDecorator";
-import { Theme } from "../../src/7_app/providers/ThemeProvider";
-import { RouterDecorator } from "../../src/1_shared/config/storybook/RouterDecorator/RouterDecorator";
+import { addDecorator } from '@storybook/react';
+import { StyleDecorator } from '../../src/1_shared/config/storybook/StyleDecorator/StyleDecorator';
+import { ThemeDecorator } from '../../src/1_shared/config/storybook/ThemeDecorator/ThemeDecorator';
+import { Theme } from '../../src/7_app/providers/ThemeProvider';
+import { RouterDecorator } from '../../src/1_shared/config/storybook/RouterDecorator/RouterDecorator';
 
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
+  actions: { argTypesRegex: '^on[A-Z].*' },
   controls: {
     matchers: {
       color: /(background|color)$/i,
-      date: /Date$/,
-    },
-  },
+      date: /Date$/
+    }
+  }
 };
 
 addDecorator(StyleDecorator);
