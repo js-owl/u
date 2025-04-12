6153e9488cfb3ee5c551a72ea2415d722e2135ad
Alex K <aklimovskikh@yandex.ru>
2025-04-12 12:42:32 +0300

    74 Add max to VStack in CommentCard.tsx

--- src/2_entities/Comment/ui/CommentCard/CommentCard.tsx
+++ src/2_entities/Comment/ui/CommentCard/CommentCard.tsx
@@ -20,13 +20,13 @@ interface CommentCardProps {
 export const CommentCard = memo(({ className, comment, isLoading }: CommentCardProps) => {
   if (isLoading) {
     return (
-      <div className={classNames(cls.CommentCard, {}, [className, cls.loading])}>
+      <VStack gap="8" max className={classNames(cls.CommentCard, {}, [className, cls.loading])}>
         <div className={cls.header}>
           <Skeleton width={30} height={30} border="50%" />
           <Skeleton width={100} height={16} />
         </div>
         <Skeleton width="100%" height={50} className={cls.text} />
-      </div>
+      </VStack>
     );
   }
 
