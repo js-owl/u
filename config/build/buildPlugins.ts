666ceba11a6f8f1afdb3ecf71a0311ceb07c5d95
Alex K <aklimovskikh@yandex.ru>
2025-04-14 19:16:11 +0300

    77 Install circular-dependency-plugin

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -3,14 +3,10 @@ import HTMLWebpackPlugin from 'html-webpack-plugin';
 import MiniCssExtractPlugin from 'mini-css-extract-plugin';
 import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer';
 import CopyPlugin from 'copy-webpack-plugin';
+import CircularDependencyPlugin from 'circular-dependency-plugin';
 import { BuildOptions } from './types/config';
 
-export function buildPlugins({
-  paths,
-  isDev,
-  apiUrl,
-  project
-}: BuildOptions): webpack.WebpackPluginInstance[] {
+export function buildPlugins({ paths, isDev, apiUrl, project }: BuildOptions): webpack.WebpackPluginInstance[] {
   const plugins = [
     new webpack.ProgressPlugin(),
     new HTMLWebpackPlugin({ template: paths.html }),
@@ -25,7 +21,8 @@ export function buildPlugins({
     }),
     new CopyPlugin({
       patterns: [{ from: paths.locales, to: paths.buildLocales }]
-    })
+    }),
+    new CircularDependencyPlugin({ exclude: /node_modules/, failOnError: true })
   ];
   if (isDev) {
     plugins.push(new webpack.HotModuleReplacementPlugin());
