0defd9388e4b1686c2d2dc944fa466215f03f72b
Alex K <aklimovskikh@yandex.ru>
2025-04-15 23:05:57 +0300

    78 Install fork-ts-checker-webpack-plugin

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -4,6 +4,7 @@ import MiniCssExtractPlugin from 'mini-css-extract-plugin';
 import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer';
 import CopyPlugin from 'copy-webpack-plugin';
 import CircularDependencyPlugin from 'circular-dependency-plugin';
+import ForkTsCheckerWebpackPlugin from 'fork-ts-checker-webpack-plugin';
 import { BuildOptions } from './types/config';
 
 export function buildPlugins({ paths, isDev, apiUrl, project }: BuildOptions): webpack.WebpackPluginInstance[] {
@@ -22,7 +23,16 @@ export function buildPlugins({ paths, isDev, apiUrl, project }: BuildOptions): w
     new CopyPlugin({
       patterns: [{ from: paths.locales, to: paths.buildLocales }]
     }),
-    new CircularDependencyPlugin({ exclude: /node_modules/, failOnError: true })
+    new CircularDependencyPlugin({ exclude: /node_modules/, failOnError: true }),
+    new ForkTsCheckerWebpackPlugin({
+      typescript: {
+        diagnosticOptions: {
+          semantic: true,
+          syntactic: true
+        },
+        mode: 'write-references'
+      }
+    })
   ];
   if (isDev) {
     plugins.push(new webpack.HotModuleReplacementPlugin());
