5b895c3bff4dbed844dae0de8537998c3df147ff
Alex K <aklimovskikh@yandex.ru>
2025-04-18 10:38:47 +0300

    80 Add MobileView in NotificationButton.tsx

--- src/3_features/notificationButton/ui/NotificationButton/NotificationButton.tsx
+++ src/3_features/notificationButton/ui/NotificationButton/NotificationButton.tsx
@@ -1,8 +1,10 @@
-import { memo } from 'react';
+import { memo, useCallback, useState } from 'react';
+import { BrowserView, MobileView } from 'react-device-detect';
+import NotificationIcon from '1_shared/assets/icons/notification-20-20.svg';
 import { Button, ButtonTheme } from '1_shared/ui/Button/Button';
 import { Icon } from '1_shared/ui/Icon/Icon';
 import { Popover } from '1_shared/ui/Popups';
-import NotificationIcon from '1_shared/assets/icons/notification-20-20.svg';
+import { Drawer } from '1_shared/ui/Drawer/Drawer';
 import { NotificationList } from '2_entities/Notification';
 import cls from './NotificationButton.module.scss';
 
@@ -10,16 +12,33 @@ interface NotificationButtonProps {
   className?: string;
 }
 export const NotificationButton = memo(({ className }: NotificationButtonProps) => {
+  const [isOpen, setIsOpen] = useState(false);
+  const onOpenDrawer = useCallback(() => {
+    setIsOpen(true);
+  }, []);
+  const onCloseDrawer = useCallback(() => {
+    setIsOpen(false);
+  }, []);
+
+  const trigger = (
+    <Button onClick={onOpenDrawer} theme={ButtonTheme.CLEAR}>
+      <Icon Svg={NotificationIcon} inverted />
+    </Button>
+  );
+
   return (
-    <Popover
-      direction="bottom left"
-      trigger={
-        <Button theme={ButtonTheme.CLEAR}>
-          <Icon Svg={NotificationIcon} inverted />
-        </Button>
-      }
-    >
-      <NotificationList className={cls.notifications} />
-    </Popover>
+    <div>
+      <BrowserView>
+        <Popover direction="bottom left" trigger={trigger}>
+          <NotificationList className={cls.notifications} />
+        </Popover>
+      </BrowserView>
+      <MobileView>
+        {trigger}
+        <Drawer isOpen={isOpen} onClose={onCloseDrawer}>
+          <NotificationList />
+        </Drawer>
+      </MobileView>
+    </div>
   );
 });
--- src/4_widgets/Navbar/ui/Navbar.tsx
+++ src/4_widgets/Navbar/ui/Navbar.tsx
@@ -1,5 +1,5 @@
 import { useCallback, useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import { useSelector } from 'react-redux';
 import { useTranslation } from 'react-i18next';
 
 import { RoutePath } from '1_shared/config/routeConfig/routeConfig';
@@ -8,10 +8,8 @@ import { Button, ButtonTheme } from '1_shared/ui/Button/Button';
 import { Text, TextTheme } from '1_shared/ui/Text/Text';
 import { AppLink, AppLinkTheme } from '1_shared/ui/AppLink/AppLink';
 import { HStack } from '1_shared/ui/Stack';
-import { Drawer } from '1_shared/ui/Drawer/Drawer';
 
-import { getUserAuthData, isUserAdmin, isUserManager, userActions } from '2_entities/User';
-import { NotificationList } from '2_entities/Notification';
+import { getUserAuthData } from '2_entities/User';
 import { LoginModal } from '3_features/AuthByUsername';
 import { NotificationButton } from '3_features/notificationButton';
 import { AvatarDropdown } from '3_features/avatarDropdown';
@@ -23,22 +21,14 @@ interface NavbarProps {
 
 export const Navbar = ({ className }: NavbarProps) => {
   const { t } = useTranslation();
-  const [isAuthModal, setIsAuthModal] = useState(false);
   const authData = useSelector(getUserAuthData);
 
+  const [isAuthModal, setIsAuthModal] = useState(false);
   const onCloseModal = useCallback(() => {
     setIsAuthModal(false);
   }, []);
   const onShowModal = useCallback(() => {
     setIsAuthModal(true);
-  }, []);
-
-  const [isOpen, setIsOpen] = useState(false);
-  const onOpenDrawer = useCallback(() => {
-    setIsOpen(true);
-  }, []);
-  const onCloseDrawer = useCallback(() => {
-    setIsOpen(false);
   }, []);
 
   if (authData) {
@@ -49,12 +39,6 @@ export const Navbar = ({ className }: NavbarProps) => {
           {t('create article')}
         </AppLink>
         <HStack gap="16" className={cls.actions}>
-          <button type="button" onClick={onOpenDrawer}>
-            {t('click')}
-          </button>
-          <Drawer isOpen={isOpen} onClose={onCloseDrawer}>
-            <NotificationList />
-          </Drawer>
           <NotificationButton />
           <AvatarDropdown />
         </HStack>
